e0951269ce003237f067b5ce60effab67ba48549
Eyal Itzhaki <eyal.itzhaki@gmail.com>
2021-07-13 15:00:04 +0300

    Update nodejs_docker_pipeline.groovy

--- jenkins/master/nodejs_docker_pipeline.groovy
+++ jenkins/master/nodejs_docker_pipeline.groovy
@@ -2,7 +2,7 @@ pipelineJob('boilerplate-pipeline') {
     definition{
         cpsScm {
             scm{
-                git('git://github.com/eitzhaki/docker-cicd.git', 'master') {  node -> // is hudson.plugins.git.GitSCM
+                git('git://github.com/eitzhaki/docker-cicd.git', 'advanced') {  node -> // is hudson.plugins.git.GitSCM
             }
             scriptPath(readFileFromWorkspace('./basic/misc/Jenkinsfile.v2'))
             }
