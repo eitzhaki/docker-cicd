a4f88f0819e0dc9988dadbf6f4feba451ea57f34
Eyal Itzhaki <eyal.itzhaki@gmail.com>
2021-07-13 14:08:13 +0300

    Update nodejs_docker_pipeline.groovy

--- jenkins/master/nodejs_docker_pipeline.groovy
+++ jenkins/master/nodejs_docker_pipeline.groovy
@@ -1,1 +1,23 @@
-
+pipelineJob('boilerplate-pipeline') {
+    scm {
+        git('git://github.com/eitzhaki/docker-cicd.git', 'master') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('DSL User')
+            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+        }
+    }
+    triggers {
+        scm('H/5 * * * *')
+    }
+    steps {
+        dockerBuildAndPublish {
+            repositoryName('eyali/test')
+            tag('${GIT_REVISION,length=9}')
+            registryCredentials('eyali-repo')
+            buildContext('basics')
+            forcePull(false)
+            forceTag(false)
+            createFingerprints(false)
+            skipDecorate()
+        }
+    }
+}
