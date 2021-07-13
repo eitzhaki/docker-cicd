9bd8d4cdc29d6af526bec5c2fa2b2acf9cb3984d
Eyal Itzhaki <eyal.itzhaki@gmail.com>
2021-07-13 11:07:38 +0300

    Update nodejs.groovy

--- jenkins/master/nodejs.groovy
+++ jenkins/master/nodejs.groovy
@@ -1,6 +1,6 @@
 job('NodeJS example') { // Job NAME
     scm { // Configure Source control management 
-        git('git://github.com/eitzhaki/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+        git('git://github.com/eitzhaki/docker-cicd.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
             node / gitConfigEmail('jenkins-dsl@domain.com')
         }
